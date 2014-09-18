cb14e75b22214222c49401f56ec606dd22d68f3e
bastiankemmer <bastian.kemmer@itizzimo.com>
2014-09-18 16:19:34 +0200

    Initial Commit

---
+++ ShowCaseApp/MainPage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using System.IO.Compression;
+using Windows.Storage.Pickers.Provider;
+using Windows.Storage.Provider;
+using Windows.Storage.Search;
+using Windows.Web;
+using Windows.Phone.UI.Input;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=391641 dokumentiert.
+
+namespace ShowCaseApp
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private string appName;
+        private StorageFolder localFolder;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            HardwareButtons.BackPressed += this.MainPage_BackPressed;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            appName = "myhtml.zip";
+            localFolder = KnownFolders.MusicLibrary;
+        }
+
+        private void MainPage_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            if (this.Frame.CanGoBack)
+            {
+                e.Handled = true;
+                this.Frame.GoBack();
+            }
+        }
+
+        private async void UnpackZipFiles()
+        {
+            var myZip = await localFolder.CreateFileAsync(appName, CreationCollisionOption.OpenIfExists);
+            using (var zipToOpen = await myZip.OpenStreamForReadAsync())
+            {
+                using (var zipMemoryStream = new MemoryStream((int)zipToOpen.Length))
+                {
+                    await zipToOpen.CopyToAsync(zipMemoryStream);
+
+                    using (var archive = new ZipArchive(zipMemoryStream, ZipArchiveMode.Read))
+                    {
+                        var folder = await localFolder.CreateFolderAsync(myZip.DisplayName + "unzip", CreationCollisionOption.ReplaceExisting);
+                        var unzipFolder = folder;
+
+                        foreach (var entry in archive.Entries)
+                        {
+                            if (entry.Name != "")
+                            {
+                                using (var fileData = entry.Open())
+                                {
+                                    var outputFile = await unzipFolder.CreateFileAsync(entry.Name, CreationCollisionOption.ReplaceExisting);
+                                    using (var outputFileStream = await outputFile.OpenStreamForWriteAsync())
+                                    {
+                                        await fileData.CopyToAsync(outputFileStream);
+                                        await outputFileStream.FlushAsync();
+                                    }
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        private void GoTo_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(WebView));
+        }
+        private void Unpacker_Click(object sender, RoutedEventArgs e)
+        {
+            UnpackZipFiles();
+        }
+    }
+}
---
+++ ShowCaseApp/WebView.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using System.IO.Compression;
+using Windows.Storage.Pickers.Provider;
+using Windows.Storage.Provider;
+using Windows.Storage.Search;
+using Windows.Web;
+using Windows.Phone.UI.Input;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkID=390556 dokumentiert.
+
+namespace ShowCaseApp
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet werden kann oder auf die innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class WebView : Page
+    {
+        public WebView()
+        {
+            this.InitializeComponent();
+            //var localFolder = KnownFolders.MusicLibrary;
+
+            //var folder = await localFolder.GetFolderAsync("myhtmlunzip");
+
+
+            //var file = await folder.GetFileAsync("index.html");
+        }
+
+        /// <summary>
+        /// Wird aufgerufen, wenn diese Seite in einem Frame angezeigt werden soll.
+        /// </summary>
+        /// <param name="e">Ereignisdaten, die beschreiben, wie diese Seite erreicht wurde.
+        /// Dieser Parameter wird normalerweise zum Konfigurieren der Seite verwendet.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            var MyUrl = MyWebViewControl.BuildLocalStreamUri("index", "myhtmlunzip/index.html");
+
+            MyWebViewControl.NavigateToLocalStreamUri(MyUrl, new MyStreamUriResolver());
+
+            HardwareButtons.BackPressed += this.MainPage_BackPressed;
+        }
+
+        private void MainPage_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            if (this.Frame.CanGoBack)
+            {
+                e.Handled = true;
+                this.Frame.GoBack();
+            }
+        }
+    }
+    public sealed class MyStreamUriResolver : IUriToStreamResolver
+    {
+        public IAsyncOperation<IInputStream> UriToStreamAsync(Uri uri)
+        {
+            lock (this)
+            {
+                if (uri == null)
+                {
+                    throw new Exception();
+                }
+                string path = uri.AbsolutePath;
+                return GetContent(path).AsAsyncOperation();
+            }
+        }
+
+        private async Task<IInputStream> GetContent(string path)
+        {
+            try
+            {
+                Uri localUri = new Uri("ms-appdata://" + path);
+                StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(localUri);
+
+                IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read).AsTask().ConfigureAwait(false);
+
+                if (true)
+                {
+                    Windows.Storage.Streams.DataReader reader = new DataReader(stream);
+                    await reader.LoadAsync((uint)stream.Size);
+                    var fileContent = reader.ReadString((uint)stream.Size);
+                    stream.Seek(0);
+                }
+                return stream;
+
+            }
+            catch
+            {
+                throw new Exception("Path is invalid");
+            }
+        }
+    }
+}
